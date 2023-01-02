f9556c5fbd914121f7172586eb2b2cffcdfc959f
saifmohamedsv <xcyberghostx1@gmail.com>
2023-01-02 17:55:53 +0200

    some more routers setup -saifmohamed

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,47 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { HelmetProvider } from 'react-helmet-async'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { HelmetProvider } from "react-helmet-async";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
-const rootElement = document.getElementById('root')
+const rootElement = document.getElementById("root");
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    // element: <App />,
+  },
+  {
+    path: "/register",
+    // element: <App />,
+  },
+  {
+    path: "/my-learning",
+    // element: <App />,
+  },
+  {
+    path: "/course/:id",
+    // element: <App />,
+  },
+  {
+    path: "/cart",
+    // element: <App />,
+  },
+]);
+
 const components = (
-    <HelmetProvider>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </HelmetProvider>
-)
+  <HelmetProvider>
+    <RouterProvider router={router} />
+  </HelmetProvider>
+);
 
-ReactDOM.createRoot(rootElement).render(components)
+ReactDOM.createRoot(rootElement).render(components);
