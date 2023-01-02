c39d1891b1159c0867469e757f518837aa677b05
naglaa77 <sama_hap@yahoo.com>
2023-01-02 22:27:26 +0100

    test naglaa

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,44 +1,43 @@
-import React from 'react'
-import { Helmet } from 'react-helmet-async'
-import udemyLogo from '../assets/udemy.svg'
+import React from "react";
+import { Helmet } from "react-helmet-async";
+import udemyLogo from "../assets/udemy.svg";
 
 export default function Home() {
-    return (
-        <div>
-            <Helmet>
-                <title>page1</title>
-            </Helmet>
-            <div className='avatar online'>
-                <div className='w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2'>
-                    <img
-                        src={udemyLogo}
-                        style={{ objectFit: 'contain' }}
-                        alt='Udemy Logo'
-                    />
-                </div>
-            </div>
-            <h1 className='m-2'>Udemy</h1>
-            <label htmlFor='my-modal' className='btn'>
-                open modal
-            </label>
-
-            <input type='checkbox' id='my-modal' className='modal-toggle' />
-            <div className='modal'>
-                <div className='modal-box'>
-                    <h3 className='font-bold text-lg'>
-                        Congratulations Udemy Clone User!
-                    </h3>
-                    <p className='py-4 '>
-                        You've been selected to be on the Team developing Udemy
-                        Clone
-                    </p>
-                    <div className='modal-action'>
-                        <label htmlFor='my-modal' className='btn'>
-                            Yay!
-                        </label>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div>
+      <Helmet>
+        <title>page2</title>
+      </Helmet>
+      <div className="avatar online">
+        <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+          <img
+            src={udemyLogo}
+            style={{ objectFit: "contain" }}
+            alt="Udemy Logo"
+          />
         </div>
-    )
+      </div>
+      <h1 className="m-2">Udemy</h1>
+      <label htmlFor="my-modal" className="btn">
+        open modal
+      </label>
+
+      <input type="checkbox" id="my-modal" className="modal-toggle" />
+      <div className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">
+            Congratulations Udemy Clone User!
+          </h3>
+          <p className="py-4 ">
+            You've been selected to be on the Team developing Udemy Clone
+          </p>
+          <div className="modal-action">
+            <label htmlFor="my-modal" className="btn">
+              Yay!
+            </label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
