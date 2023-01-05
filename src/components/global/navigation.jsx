a5d61f6fbaf553c5825241da3102a620759a71d4
siham heddi <siham.heddi.dev@gmail.com>
2023-01-05 12:20:26 +0100

    navbar version1

--- src/components/global/navigation.jsx
+++ src/components/global/navigation.jsx
@@ -2,7 +2,8 @@
 import udemyLogo from '../../assets/udemy.svg'
 
 export default function Navigation() {
-    return (<div className="navbar bg-base-100">
+    return (<div>
+        <div className="navbar bg-base-100 border-b border-gray-300">
                 <div className="flex-1">
                      <div className='w-40'>
                         <a href="/" >
@@ -13,7 +14,7 @@ export default function Navigation() {
                                 />
                         </a>
                      </div>
-                     <div className='mx-2'><a href='#'>Categories</a></div>
+                     {/* <div className='mx-2'><a href='#'>Categories</a></div> */}
                      <div className='mx-6 w-1/2'> 
                             <div class="form-control">
                               <input type="text" placeholder="Search for everything" class="rounded-3xl bg-gray-50	 focus:outline-0 input input-bordered border-zinc-700 text-zinc-700" />
@@ -28,16 +29,41 @@ export default function Navigation() {
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
                             </svg>
                           </label>
-                        <div tabindex="0" class="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow">
-                            <div class="card-body">
-                               <p>
-                                ksksksksd kdskdskds
-                               </p>
+                       {/* content of card */}
+                       <div tabindex="0" class="mt-3  card card-compact  dropdown-content w-52 bg-base-100 shadow border border-grey-400 w-96 rounded">
+                            <div class="card-body mt-4 ">
+                                {/* product 1*/ }
+                                   <div className="mb-3">
+                                        <div className="flex mb-3">
+                                                        <div class="w-20">
+                                                            <img src="https://placeimg.com/80/80/people"/>
+                                                        </div>
+                                                        <div className='ml-3'>
+                                                            <h3 className='font-bold text-base'><a href='#'>Lorem, ipsum dolor sit amet consectetur </a></h3>
+                                                            <p className='text-gray-400 my-1'>Robin jesper</p >
+                                                            <p> <span className='text-sm font-bold'>$ 9.00</span> <span className='pl-1 line-through text-gray-500'>$ 10.00</span></p>
+                                                        </div>
+                                        </div>
+                                        <button className='btn btn-outline rounded w-full'>
+                                            Add to card
+                                        </button>
+                                   </div>
+                                   
+                               {/* actions */}
+
+                                <div className="border-t ">
+                                      <button className='btn btn-dark rounded w-full mt-4'>
+                                            Go to wishlist
+                                        </button>
+                                </div>
+
+
+
                             </div>
                        </div>
                     </div>
 
-
+                    {/* cards info */}
                     <div class="dropdown dropdown-end ml-3">
                          <label tabindex="0" class="btn btn-ghost btn-circle">
                             <div class="indicator">
@@ -47,42 +73,151 @@ export default function Navigation() {
                                     <span class="badge badge-sm badge-primary indicator-item">8</span>
                             </div>
                           </label>
-                        <div tabindex="0" class="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow">
-                            <div class="card-body">
-                                <span class="font-bold text-lg">8 Items</span>
-                                <span class="text-info">Subtotal: $999</span>
-                                <div class="card-actions">
-                                    <button class="btn btn-primary btn-block">View cart</button>
+                          {/* content of card */}
+                        <div tabindex="0" class="mt-3  card card-compact  dropdown-content w-52 bg-base-100 shadow border border-grey-400 w-96 rounded">
+                            <div class="card-body mt-4 ">
+                                {/* product 1*/ }
+                                    <div className="flex mb-2">
+                                                <div class="w-20">
+                                                    <img src="https://placeimg.com/80/80/people"/>
+                                                </div>
+                                                <div className='ml-3'>
+                                                    <h3 className='font-bold text-base'><a href='#'>Lorem, ipsum dolor sit amet consectetur </a></h3>
+                                                    <p className='text-gray-400 my-1'>Robin jesper</p >
+                                                    <p> <span className='text-sm font-bold'>$ 9.00</span> <span className='pl-1 line-through text-gray-500'>$ 10.00</span></p>
+                                                </div>
+                                    </div>
+                               {/* actions */}
+
+                                <div className="border-t ">
+                                    <p className='mt-4'><span className='font-bold text-xl'>Total : 9.00 $</span> <span className='pl-1 line-through text-gray-500'>$ 10.00</span></p>
                                 </div>
+
+
+
                             </div>
                        </div>
                     </div>
-                    <button class="btn btn-ghost btn-circle ml-3">
-                        <div class="indicator">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
-</svg>
 
-                            <span class="badge badge-xs badge-primary indicator-item"></span>
-                        </div>
-                     </button>
+
+                     {/* notifications info */}
+
+                    <div class="dropdown dropdown-end ml-3">
+                         <label tabindex="0" class="btn btn-ghost btn-circle">
+                            <div class="indicator">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
+                                </svg>
+
+                                <span class="badge badge-xs badge-primary indicator-item"></span>
+                            </div>
+                          </label>
+                        <div tabindex="0" class="mt-3 card card-compact  dropdown-content w-52 bg-base-100 shadow border border-grey-400 w-96 rounded">
+                            <div class="card-body">
+                              
+                              <div className="flex text-lg">
+                                <h1 className='font-bold'>Notifications</h1>
+                                <h2 className="ml-auto font-bold text-primary"><a href="">Settings</a></h2>
+                              </div>
+                                {/* notifications */}
+                             <div className=' mt-5'>
+                                {/* notification 1 */}
+                                <div className="flex">
+                                        <div class="w-20">
+                                            <img src="https://placeimg.com/80/80/people" className='rounded-full'/>
+                                        </div>
+                                        <div className='ml-3'>
+                                            <h3 className='font-bold text-base'><a href='#'>Lorem, ipsum dolor sit amet consectetur </a></h3>
+                                            <p className='text-gray-500 text-sm'>15 Hours ago</p>
+                                        </div>
+                                </div>
+                             </div>
+
+
+                              {/*actions */}
+                             <div className="mt-4 border-t border-zinc-200 flex justify-between py-2	mb-0">
+                                <a href="#" className='font-bold mt-4 ml-2'>Mark all as read</a>
+                                <a href="#" className='btn btn-outline mt-2 rounded'>See all</a>
+
+                             </div>
+                              
+                            </div>
+
+                       </div>
+                    </div>
+
+
+                    {/* profile actions */}
                      <div class="dropdown dropdown-end ml-3">
                         <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+                            
                             <div class="w-10 rounded-full">
-                            <img src="https://placeimg.com/80/80/people" />
+                                <img src="https://placeimg.com/80/80/people" />
+                                <span class="badge badge-xs badge-primary indicator-item"></span>
+
                             </div>
                         </label>
-                        <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li>
-                            <a class="justify-between">
-                                Profile
-                                <span class="badge">New</span>
-                            </a>
+                        <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 border border-grey-400 w-72">
+                            <li className='border-b border-grey-600'>
+                                <div className="flex-none p-3 ">
+                                    <div >
+                                            <img src="https://placeimg.com/80/80/people"  className="avatar w-30 rounded-full"/>
+
+                                    </div>
+                                    <div>
+                                        <h1 className='text-lg font-bold'>Full Name</h1>
+                                        <p className='text-gray-500	'>email@email.com</p>
+                                    </div>
+                                </div>
                             </li>
-                            <li><a>Settings</a></li>
-                            <li><a>Logout</a></li>
+                            <li className='border-b border-grey-600'>
+                                <a href='#' className='mt-2'>My learning</a>
+                                <a href='#' className='mt-2'>My cart</a>
+                                <a href='#' className='mt-2'>Wishlist</a>
+
+                            </li>
+                            <li className='border-b border-grey-600'>
+                                <a href='#' className='mt-2'>Notification</a>
+                                <a href='#' className='mt-2'>Messages</a>
+                            </li>
+                            <li className='border-b border-grey-600'>
+                                <a href='#' className='mt-2'>Account settings</a>
+                                <a href='#' className='mt-2'>Purchase history</a>
+                            </li>
+                            <li className='border-b border-grey-600'>
+                           
+                                <a href='#' className='mt-2'>Edit Profile</a>
+                            </li>
+                            <li >
+                                <a href='#' className='mt-2'>Logout</a>
+                            </li>
                         </ul>
                       </div>
                  </div>
-            </div>)
+            </div>
+            <div className="navbar bg-base-100 shadow">
+                <div className="flex-1 container mx-auto ">
+                    <div>
+                        <a href="#" className='mr-5'>Developement</a>
+                    </div>
+                    <div>
+                        <a href="#" className='mr-5'>IT & softawre</a>
+                    </div>
+                    <div>
+                        <a href="#" className='mr-5'>Health</a>
+                    </div>
+                    <div>
+                        <a href="#" className='mr-5'>Finance</a>
+                    </div>
+                    <div>
+                        <a href="#" className='mr-5'>Office productivity</a>
+                    </div>
+                    <div>
+                        <a href="#" className='mr-5'>Personal developement</a>
+                    </div>
+                </div>
+            </div>
+
+
+    </div>)
 }
