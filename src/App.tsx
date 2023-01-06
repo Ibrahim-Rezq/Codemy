b7f3a29ba153b8094190fa548247614e7f276fcc
naglaa77 <sama_hap@yahoo.com>
2023-01-06 16:34:10 +0100

    finish hero section but with error in image responsive for mobile screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Hero from './components/heroSlider/Hero'
 
 import { routes } from './utils/routingConsts'
+
 const router = createBrowserRouter(routes)
 
 export default function App() {
     return (
         <div className="App">
+            <Hero />
             <RouterProvider router={router} />
         </div>
     )
