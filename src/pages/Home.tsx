9b2fa9bb699a6542e7f7ee8349866a287473a91e
Ibrahim Rezq <90980686+Ibrahim-Rezq@users.noreply.github.com>
2023-01-09 18:16:34 +0200

    merge conflicts fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,7 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
-<<<<<<< HEAD
-import { Container, CourseCard, SEOHead } from '../components'
-=======
 import { Container, CourseCard, PageTitle, SEOHead } from '../components'
->>>>>>> e8bd4b907479452e05dc036b357fecf7d540cca6
 import AboutUs from '../components/home/AboutUs'
 import Hero from '../components/home/Hero'
 import { selectUser } from '../redux/features/user/userSlice'
@@ -19,11 +15,6 @@ const Home = () => {
             <SEOHead title="Main Page" />
             <Hero />
             <Container>
-<<<<<<< HEAD
-                <SEOHead title="Main Page" />
-                <Hero />
-                <CourseCard />
-=======
                 <PageTitle title={'It & Software'} />
                 <div className="grid grid-cols-5 gap-2 my-10">
                     {[...Array(5)].map(() => {
@@ -44,7 +35,6 @@ const Home = () => {
                         )
                     })}
                 </div>
->>>>>>> e8bd4b907479452e05dc036b357fecf7d540cca6
                 <AboutUs />
             </Container>
         </div>
