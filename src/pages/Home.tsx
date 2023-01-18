a64e588d2b073858838a2eaff41e558831855365
naglaa77 <sama_hap@yahoo.com>
2023-01-18 15:44:28 +0100

    change

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,7 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
-<<<<<<< HEAD
-import { Container, CourseCard, SEOHead } from '../components'
-=======
 import { Container, CourseCard, PageTitle, SEOHead } from '../components'
->>>>>>> e8bd4b907479452e05dc036b357fecf7d540cca6
 import AboutUs from '../components/home/AboutUs'
 import Hero from '../components/home/Hero'
 import { selectUser } from '../redux/features/user/userSlice'
@@ -19,17 +15,12 @@ const Home = () => {
             <SEOHead title="Main Page" />
             <Hero />
             <Container>
-<<<<<<< HEAD
-                <SEOHead title="Main Page" />
-                <Hero />
-                <CourseCard />
-=======
                 <PageTitle title={'It & Software'} />
                 <div className="grid grid-cols-5 gap-2 my-10">
                     {[...Array(5)].map(() => {
                         return (
                             <>
-                                <CourseCard />
+                                <CourseCard wide={false} />
                             </>
                         )
                     })}
@@ -39,12 +30,11 @@ const Home = () => {
                     {[...Array(5)].map(() => {
                         return (
                             <>
-                                <CourseCard />
+                                <CourseCard wide={false} />
                             </>
                         )
                     })}
                 </div>
->>>>>>> e8bd4b907479452e05dc036b357fecf7d540cca6
                 <AboutUs />
             </Container>
         </div>
