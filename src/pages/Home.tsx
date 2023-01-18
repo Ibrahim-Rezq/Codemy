e6774ff8d52fd560fb27777e923edac82bb82c80
TarekSaeed95 <110253624+TarekSaeed95@users.noreply.github.com>
2023-01-18 21:35:49 +0200

    courseSlice before merging

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,19 +9,13 @@ import { selectUser } from '../redux/features/user/userSlice'
 const Home = () => {
     const state = useSelector(selectUser)
     const dispatch = useDispatch()
-    const filter = useSelector(filterState)
-    const course = useSelector(coursesState)
-    const filteredCourse = useSelector(filteredCoursesState)
-    console.log(filter)
-    console.log(course)
-    console.log(filteredCourse)
-    console.log(filterState)
     return (
         <div className="bg-white w-screen min-h-screen text-black">
             <SEOHead title="Main Page" />
             <Hero />
             <Container>
                 <PageTitle title={'It & Software'} />
+
                 <div className="grid grid-cols-5 gap-2 my-10">
                     {[...Array(5)].map(() => {
                         return (
