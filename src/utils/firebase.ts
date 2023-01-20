e03cd54186f07c5872510ed8aefaf86caa5439ff
Abdel-rhman <abdo784@gmail.com>
2023-01-20 14:52:22 +0200

    Firebase config update

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,8 +1,8 @@
 // fire base init file
-import { getAnalytics } from 'firebase/analytics'
 import { initializeApp } from 'firebase/app'
 import { connectAuthEmulator, getAuth } from 'firebase/auth'
 import { connectFunctionsEmulator, getFunctions } from 'firebase/functions'
+import { connectFirestoreEmulator, getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
     apiKey: 'AIzaSyA8EEsPA_dqx04K_v7fMTAOtYg4GJvF4Gs',
@@ -14,15 +14,20 @@ const firebaseConfig = {
 }
 
 const app = initializeApp(firebaseConfig)
-const analytics = getAnalytics(app)
 export const auth = getAuth(app)
 export const functions = getFunctions(app)
 
 connectAuthEmulator(auth, 'http://localhost:9099')
 connectFunctionsEmulator(functions, 'localhost', 5001)
 
+const db = getFirestore(app)
+connectFirestoreEmulator(db, 'localhost', 8080)
+
+
+
 //9099 auth
 //5001 func
 //8080 store
 //5000 host
 //5500 hoUI
+
