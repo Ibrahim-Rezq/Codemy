7f98c01ec6f541d5d24412f450c9c56d5cf4dfa2
Ibrahim Rezq <90980686+Ibrahim-Rezq@users.noreply.github.com>
2023-01-22 16:22:09 +0200

    input update

--- src/components/global/InputField.tsx
+++ src/components/global/InputField.tsx
@@ -1,36 +1,39 @@
 import { useState } from 'react'
 import { FaEye } from 'react-icons/fa'
 
-function InputField({ type, label = 'Full Name' }: { type: 'text' | 'password' | 'email'; label?: string }) {
+interface InputType extends React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {}
+
+const InputField = (props: InputType) => {
     const [passwordShown, setPasswordShown] = useState(false)
 
     return (
         <div className="relative w-full flex border border-black">
             <input
                 className="w-full  pt-6 h-16 pl-4 focus:outline-0 peer"
-                type={passwordShown ? 'text' : type}
-                name={type}
-                id={type}
+                type={passwordShown ? 'text' : 'text'}
+                name={'text'}
+                id={'text'}
                 placeholder=""
+                {...props}
             />
-            {type == 'password' && (
+            {'password' == 'password' && (
                 <FaEye
                     onClick={() => setPasswordShown((prev) => !prev)}
                     className="font-bold text-xl mt-5 mr-4 cursor-pointer"
                 />
             )}
-            {label && (
+            {
                 <label
-                    htmlFor={type}
+                    htmlFor={'text'}
                     className="absolute text-xs top-1/2 font-bold -translate-y-6 -translate-x-1 left-0 pl-4 duration-100 
                 peer-focus:-translate-y-6 peer-focus:-translate-x-1 peer-focus:text-xs 
                 peer-placeholder-shown:-translate-y-1/2 
                 peer-placeholder-shown:font-bold
                 peer-placeholder-shown:text-sm capitalize"
                 >
-                    {label}
+                    {'label'}
                 </label>
-            )}
+            }
         </div>
     )
 }
